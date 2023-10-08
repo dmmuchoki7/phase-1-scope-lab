c37b91bc039a5c53cbba3b6b650708ae25553249
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-08 18:46:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName= "bob";
+const leastFavoriteCustomer = 'kelvin';
+
+//function to uppercase the global variable
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    //console.log(customerName);
+}
+
+//declare a global variable in a function
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+//
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+//try change leastFavourite customer
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'brian';
+}
+upperCaseCustomerName();
+
+
